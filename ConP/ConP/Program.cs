9c1c1510f0cc69ff4ea8e9329d10f62e90fc9239
SMS669 <saho7@student.eadania.dk>
2025-09-15 12:15:46 +0200

    hej

--- ConP/ConP/Program.cs
+++ ConP/ConP/Program.cs
@@ -45,5 +45,73 @@ namespace Testing
             Console.WriteLine("Type the number corresponding to the weapon and the CPU will randomly choose it's weapon. \n1 - Rock, 2 - Paper, 3 - Scissor");
 
         }
+        // koden notere hvor mange gange der gættes forkert 
+        private static void printHangman(int wrong)
+
+        {
+            if (wrong == 0) // if statement for at notere hver gang der gættes forkert med en illustration 
+            {
+                Console.WriteLine("\n+---+ ");
+                Console.WriteLine("     |  ");
+                Console.WriteLine("     |  ");
+                Console.WriteLine("     |  ");
+                Console.WriteLine("    ---  ");
+            }
+            else if (wrong = 1)
+            {
+                Console.WriteLine("\n+---+ ");
+                Console.WriteLine("  O  |  ");
+                Console.WriteLine("     |  ");
+                Console.WriteLine("     |  ");
+                Console.WriteLine("    ---  ");
+            }
+            else if (wrong = 2)
+            {
+                Console.WriteLine("\n+---+ ");
+                Console.WriteLine("  O  |  ");
+                Console.WriteLine("  |  |  ");
+                Console.WriteLine("     |  ");
+                Console.WriteLine("    ---  ");
+            }
+            else if (wrong = 3)
+            {
+                Console.WriteLine("\n+---+ ");
+                Console.WriteLine("  O  |  ");
+                Console.WriteLine(" /|  |  ");
+                Console.WriteLine("     |  ");
+                Console.WriteLine("    ---  ");
+            }
+            else if (wrong = 4)
+            {
+                Console.WriteLine("\n+---+ ");
+                Console.WriteLine("  O  |  ");
+                Console.WriteLine(" /|\\|  "); // doubble backslash for at komme ud af den da det er en funktion ellers 
+                Console.WriteLine("     |  ");
+                Console.WriteLine("    ---  ");
+            }
+            else if (wrong = 5)
+            {
+                Console.WriteLine("\n+---+ ");
+                Console.WriteLine("  O  |  ");
+                Console.WriteLine(" /|\\|  ");
+                Console.WriteLine(" /   |  ");
+                Console.WriteLine("    ---  ");
+            }
+            else if (wrong = 6)
+            {
+                Console.WriteLine("\n+---+ ");
+                Console.WriteLine("  O  |  ");
+                Console.WriteLine(" /|\\|  ");
+                Console.WriteLine(" / \\|  ");
+                Console.WriteLine("    ---  ");
+            }
+
+        }
+        private static int printWord(List<char> guessedLetters, string randomWord) // 2 parametre indført som  er et bogstav vores gættet ord og anden er vores tilfældige ord vi har inddraget. 
+        {
+            int counter = 0;
+            int rightLetters = 0;
+            Console.WriteLine();
+        }
     }
 }

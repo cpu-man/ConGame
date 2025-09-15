6ef10e163ccd9c0f20f4d3341ad9299b441d66be
cpu-man <71815606+cpu-man@users.noreply.github.com>
2025-09-15 10:37:17 +0200

    test

---
+++ ConP/ConP/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName;
+            byte userAge = 18;
+            bool validAge = true;
+            Console.WriteLine("Please type your name");
+            userName = Console.ReadLine();
+            do
+            {
+                Console.WriteLine("Please type your age");
+                userAge = Convert.ToByte(Console.ReadLine());
+                if (userAge < 18)
+                {
+                    Console.WriteLine("You're sadly not old enough to use this platform");
+                    validAge = false;
+                }
+                else
+                {
+                    validAge = true;
+                }
+
+            } while (!validAge);
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine($"WELCOME {userName} TO THE CONSOLE");
+        }
+
+        static void RockPaper()
+        {
+
+        }
+    }
+}
---
+++ ConP/ConP/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ConP")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConP")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4aa31fd9-8644-4e07-8de5-8e86168d1453")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

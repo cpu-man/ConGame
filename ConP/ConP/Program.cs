825ccae2456333425960d5e0d221d7fe0957e065
cpu-man <71815606+cpu-man@users.noreply.github.com>
2025-09-16 12:23:55 +0200

    Comments1

--- ConP/ConP/Program.cs
+++ ConP/ConP/Program.cs
@@ -11,27 +11,28 @@ namespace Testing
     {
         static void Main(string[] args)
         {
+            // Login system til konsollen hvor spilleren kan indsætte sit navn og alder
             string userName;
-            byte userAge = 18;
+            byte userAge = 18; // Minimum alder sat til 18
             bool validAge = true;
             Console.WriteLine("Please type your name");
             userName = Console.ReadLine();
             do
             {
                 Console.WriteLine("Please type your age");
-                userAge = Convert.ToByte(Console.ReadLine());
+                userAge = Convert.ToByte(Console.ReadLine()); //Konverterer spillerens input til en Byte og lægger den oveni userAge
                 if (userAge < 18)
                 {
                     Console.WriteLine("You're sadly not old enough to use this platform");
-                    validAge = false;
+                    validAge = false; 
                 }
                 else
                 {
-                    validAge = true;
+                    validAge = true; //Hvis spilleren er 18 eller over kører den videre til StartMenu funktionen
                 }
 
-            } while (!validAge);
-            StartMenu(userName);
+            } while (!validAge); // Do while loop kører så længe validAge bool er falsk, hvilket den er så længe spilleren er under 18
+            StartMenu(userName); // Kalder på StartMenu funktionen
 
         }
 
@@ -44,50 +45,53 @@ namespace Testing
             RockPaper(name);
         }
 
-        static void RockPaper(string name)
+        static void RockPaper(string name) //Seperat funktion til sten,saks, papir
         {
-            Console.Clear();
-            string[] gameObject = { "Rock", "Paper", "Scissors" };
+            Console.Clear(); //Fjerner det tidligere tekst
+            string[] gameObject = { "Rock", "Paper", "Scissors" }; //En string med flere værdier som indeholde spillets muligheder
             bool tryAgain = true;
-            Console.BackgroundColor = ConsoleColor.Magenta;
-            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Magenta; //Skifter farve på konsollens baggrund
+            Console.ForegroundColor = ConsoleColor.White; //Skifter farve på konsollens tekst
             Console.WriteLine($"Welcome to Rock, Paper & Scissors, {name}");
             Console.WriteLine("Type the number corresponding to the weapon and the CPU will randomly choose it's weapon. \n1 - Rock, 2 - Paper, 3 - Scissors \nPress any key when you're ready.");
             Console.ReadKey();
 
             do
             {
+                /* Do while loop som kører så længe vores bool er 'true'. 
+                 * 'CPU'en vælger et tilfældigt tal fra vores array og trækker én fra så det matcher vores array
+                 */
                 Console.Clear();
                 Random random = new Random();
                 int randomObject = random.Next(gameObject.Length);
                 string cpuChoice = gameObject[randomObject];
                 Console.WriteLine("Choose a weapon");
-                int userInput = int.Parse(Console.ReadLine());
-                string userChoice = gameObject[userInput - 1];
+                int userInput = int.Parse(Console.ReadLine()); //spillerens input bliver konverteret til en int vha. Parse
+                string userChoice = gameObject[userInput - 1]; // Ét tal bliver trækket fra spillerens input så det passer til arrays position
                 Console.WriteLine($"{name} chose {userChoice}");
                 Console.WriteLine($"CPU chose {cpuChoice}");
 
                 if (userChoice == cpuChoice)
                 {
-                    Console.WriteLine("It's a draw!");
+                    Console.WriteLine("It's a draw!"); // Hvis spillerens og CPU'ens valg er det samme står det lige
                 }
                 else if ((userChoice == gameObject[0] && cpuChoice == gameObject[2]) ||
                         (userChoice == gameObject[1] && cpuChoice == gameObject[0]) ||
                         (userChoice == gameObject[2] && cpuChoice == gameObject[1]))
                 {
-                    Console.WriteLine($"{name} wins the game!");
+                    Console.WriteLine($"{name} wins the game!"); //Hvis spillerens har valgt det stærkere våben og CPU'en det svagere, vinder spilleren
                 }
                 else
                 {
-                    Console.WriteLine($"CPU wins the game!");
+                    Console.WriteLine($"CPU wins the game!"); // Alle andre muligheder resulterer i CPU'en vinder
                 }
                 Console.WriteLine("Want to play again? (y/n)");
-                string userResponse = Console.ReadLine().ToLower();
-                tryAgain = (userResponse == "y");
+                string userResponse = Console.ReadLine().ToLower(); //Spillerens input bliver konverteret til lower case
+                tryAgain = (userResponse == "y"); // Hvis spilleren skriver "y" kører loopet, hvis ikke, går den ud
 
 
             } while (tryAgain);
-            StartMenu(name);
+            StartMenu(name); //Går tilbage til StartMenu funktionen
 
         }
 
